d67f8d9ca676de80d436a20a9ab4f36d3c8c4411
Zach Collins <collinzy@mail.uc.edu>
2016-10-19 13:43:09 -0400

    make perl script

---
+++ pythonify.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+# pythonify!
+
+use warnings;
+
+my ($inputFile, $outputFile) = @ARGV;
+
+if ((not defined $inputFile) or (not defined $outputFile)) {
+    die "\nUsage: perl pythonify.pl <inputfile> <outputFile>";
+}
+
+open(my $ifh, '<:encoding(UTF-8)', $inputFile) or die "\nCould not open file '$inputFile'";
+open(my $ofh, '>', $outputFile);
