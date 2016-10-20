614bb4f41c4786d53358e58bb5f243c9af982e52
Zach Collins <collinzy@mail.uc.edu>
2016-10-20 15:00:30 -0400

    update sample output

---
+++ sampleoutput.cpp
@@ -0,0 +1,21 @@
+//'sample.cpp' pythonified!
+#include <iostream>
+#include <thread>
+
+// Forward declare main
+int main()                                                                      ;
+
+void bomb()                                                                     {
+    // Just call the main function again
+    main()                                                                      ;
+    // This statement is never reached
+    std::cout << "exiting" << std::endl                                         ;}
+
+int main()                                                                      {
+    // Spawn 2 threads
+    std::thread first(bomb)                                                     ;
+    std::thread second(bomb)                                                    ;
+    // Wait for threads to finish (they never do)
+    first.join()                                                                ;
+    second.join()                                                               ;
+    return 0                                                                    ;}
